ecb66586c92e26580065984d7a9ac7e5d35933d5
CHEN Dongmei <DCHEN@lcog.org>
2021-04-09 16:38:07 -0700

    plotted the new dev raster

--- data/utilities_geopandas.py
+++ data/utilities_geopandas.py
@@ -6,10 +6,28 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
 from matplotlib.ticker import ScalarFormatter
 import rasterio
+import fiona
+from rasterio.plot import show, show_hist
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+from rasterio.mask import mask
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 path = r'T:\Trans Projects\Model Development\UrbanSim_LandUse\Output\Simulation_47_Final_RTP'
+MPObd = gpd.read_file("V:/Data/Transportation/MPO_Bound.shp")
+outpath = r'T:\Models\StoryMap\UrbanSim'
+
+class MidpointNormalize(mpl.colors.Normalize):
+    def __init__(self, vmin, vmax, midpoint=0, clip=False):
+        self.midpoint = midpoint
+        mpl.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        normalized_min = max(0, 1 / 2 * (1 - abs((self.midpoint - self.vmin) / (self.midpoint - self.vmax))))
+        normalized_max = min(1, 1 / 2 * (1 + abs((self.vmax - self.midpoint) / (self.midpoint - self.vmin))))
+        normalized_mid = 0.5
+        x, y = [self.vmin, self.midpoint, self.vmax], [normalized_min, normalized_mid, normalized_max]
+        return np.ma.masked_array(np.interp(value, x, y))
 
 def exportNewDevData(yrbuilt = 2021, by = 'yearly', step = 1):
     newDev = gpd.read_file(os.path.join(path, 'new_developments.shp'))
